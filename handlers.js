c08975975e6013661657e86131edd1b836c61297
EyePatch128 <46873888+EyePatch128@users.noreply.github.com>
2021-04-30 18:46:36 +0200

    yay

--- handlers.js
+++ handlers.js
@@ -6,11 +6,26 @@ const {joinChannel, searchMusic, fetchPlaylist, fetchLive} = require("./helpers"
 
 
 const handleHelp = msg =>{
-    msg.reply("Help")
+    msg.channel.send(`
+    Youtube dicord bot
+    Note: What's between () may not be required\n
+    \`\`\`
+    !play <song>(--artist <name>) | --url <url>: Play a song. With no arguments, resume or play next song in queue
+    !cue <song>(--artist <name>) | --url <url>: Add a song to queue
+    !playlist <url> : Add playlist. Personal playlists may not be accepted
+    !resume : Resume song
+    !pause : Pause song
+    !stop : Stop music
+    !join <voice channel> : Join channel. If no channel entered, join user's current voice channel
+    !leave : Leave voice channel
+    !live <url> : Play livestream. Should be in voice channel before
+    !skip : Skip song and play next one
+    \`\`\`
+    `)
 }
 
 const handleError = msg =>{
-    msg.reply("Please refer to Help via `!help`")
+    msg.channel.send("Please refer to Help via `!help`")
 }
 
 const handleJoin = async (msg, args, emitter) =>{
